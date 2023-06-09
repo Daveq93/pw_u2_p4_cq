f198219ae0db7eae4ecdac5d4c1380257dd011c0
Daveq93 <cdavidq.22@gmail.com>
2023-06-08 19:45:18 -0500

    Directiva v-on en Vue.JS

---
+++ info.md
@@ -0,0 +1,33 @@
+# VUE
+en html se usa las doble llaves para que interprete js
+
+
+# Option API
+permite declarar propiedades reactivas
+data me permite vincular informacion de la vista con mi programacion js
+
+# MVC
+Model (Modelo)
+View (Vista)
+Controller (Controlador)
+
+La vista va a iniciar peticiones a alguien que va a controlar las peticiones
+
+Toda la info de la vista del Estudiante, que envia hacia 
+el controlador, es conocido como el modelo
+
+Las propiedades reactivas sirven para manejar un modelo
+
+
+# Directivas
+1) para los eventos se conoce  como v-on
+    this se necesita dentro de JS, no en HTML
+
+# SPA
+Single Page Aplication
+  Voy a menejar una unica pagina.
+  
+  La navegacion tradicional demora menos en la carga inicial
+  en SPA solo la carga inicial es tardada
+
+COn SPA se mejora la experiencia de usuario
--- js/app.js
+++ js/app.js
@@ -1,21 +1,41 @@
 console.log("componente vue")
+//Options API
 console.log(Vue)
 
 const app = Vue.createApp({
-/*Aqui se van a configurar las opciones (options API)*/ 
-//opcion 1,
-//opcion 2,
-//.
-//.
-//.
-//option n
+    /*Aqui se van a configurar las opciones (options API)*/
+    //opcion 1,
+    //opcion 2,
+    //.
+    //.
+    //.
+    //option n
 
-//una de esas opciones es el template
-template:`
-<h1> Hola mundo</h1>
-<p> Desde Vue.js</p>
-`
+    //una de esas opciones es el template
+    // template: `
+    //<h1> Hola mundo</h1>
+    //<p> Desde Vue.js</p>
+    //<p>{{1+1}}</p>
+    //`
+    data() {
+        return {
+            nombre: 'Dave',
+            apellido: 'Quizhpe',
+            mensaje: 'Hola mundo 2 desde Vue.JS'
+        }
+    },
 
+    methods:{
+        cambiarNombre(){
+       console.log("Boton cambiar nombre");
+       this.nombre = 'Cristian'
+        },
+        cambiarApellido(){
+            console.log("Boton cambiar apellido");
+            this.apellido = 'Macas'
+        }
+
+    }
 
 })
 
